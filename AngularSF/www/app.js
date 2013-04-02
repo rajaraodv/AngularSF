3d16bccf0b74a8c3ce6952fac2052023b4917df6
Raja Rao DV <rajaraodv100@yahoo.com>
2013-04-02 10:42:17 -0700

    refactor

--- AngularSF/www/app.js
+++ AngularSF/www/app.js
@@ -1,40 +1,45 @@
 /**
  * bootStrapAngular is where you setup your angular app. For example,
- * the below create 'MyOppApp' app (module) and adds 'AngularForce', 'AngularForceObject' and
+ * the below create 'MyOppApp' app (module) and adds 'AngularForce', 'AngularForceObjectFactory' and
  * 'Opportunity' modules as its dependencies
  *
  * PS: This function is called when jquery.ready is triggered in index.html
  */
 function bootStrapAngular() {
-    var angularApp = angular.module('MyOppApp', ['AngularForce', 'AngularForceObject', 'Opportunity']);
+    var angularApp = angular.module('MyOppApp', ['AngularForce', 'AngularForceObjectFactory', 'Opportunity']);
     angularApp.constant('SFConfig', {});
     angularApp.config(function ($routeProvider) {
         $routeProvider.when('/edit/:oppId', { templateUrl: '#edit', onActivate: 'getOpportunity(oppId)'});
     });
-
     //load 'opportunity' module
     angular.bootstrap(document, ['MyOppApp']);
+
 }
 
 /**
- *
  * Describe Salesforce object to be used in the app. For example: Below AngularJS factory shows how to describe and
  * create an 'Opportunity' object. And then set its type, fields, where-clause etc.
+ *
  *  PS: This module is injected into ListCtrl, EditCtrl etc. controllers to further consume the object.
  */
-angular.module('Opportunity', []).factory('Opportunity', function (AngularForceObject) {
-    return AngularForceObject({type: 'Opportunity', fields: ['Name', 'ExpectedRevenue', 'StageName', 'CloseDate', 'Id'], where: 'WHERE IsWon = TRUE'});
+angular.module('Opportunity', []).factory('Opportunity', function (AngularForceObjectFactory) {
+    var Opportunity = AngularForceObjectFactory({type: 'Opportunity', fields: ['Name', 'ExpectedRevenue', 'StageName', 'CloseDate', 'Id'], where: 'WHERE IsWon = TRUE'});
+    return Opportunity;
 });
 
 /**
  * List Controller function controls JQM list view. It handles actions like object 'query', 'edit' & 'logout' etc
  *
  * @param $scope  AngularJS Scope Object
- * @param Opportunity Salesforce Object described as AngularJS Module that returns AngularForceObject
+ * @param Opportunity AngularJS module that represents an actual Salesforce Object Class
  * @param $location  AngularJS Location service - Used to change JQM page
+ * @param AngularForce AngularJS + forcetk glue - Used for AJAX calls
  * @constructor
  */
-function ListCtrl($scope, Opportunity, $location) {
+function ListCtrl($scope, Opportunity, $location, AngularForce) {
+
+    //Set login details
+    AngularForce.setCordovaLoginCred();
 
     //Query list of Opportunities
     $scope.query = function () {
@@ -51,8 +56,10 @@ function ListCtrl($scope, Opportunity, $location) {
 
     // Logout
     $scope.logout = function () {
-        var sfOAuthPlugin = cordova.require("salesforce/plugin/oauth");
-        sfOAuthPlugin.logout();
+        if (cordova) {
+            var sfOAuthPlugin = cordova.require("salesforce/plugin/oauth");
+            sfOAuthPlugin.logout();
+        }
     };
 
     //Query immediately AFTER ListCtrl is loaded (i.e. wrap inside setTimeout)
@@ -67,7 +74,7 @@ function ListCtrl($scope, Opportunity, $location) {
  * @param $location  AngularJS Location service - Used to change JQM page
  * @param $routeParams AngularJS Route obj - Contains route url's parameters
  * @param AngularForce AngularJS + forcetk glue - Used for AJAX calls
- * @param Opportunity Salesforce Object described as AngularJS Module that returns AngularForceObject
+ * @param Opportunity Salesforce Object described as AngularJS Module that returns AngularForceObjectFactory
  * @constructor
  */
 function EditCtrl($scope, $location, $routeParams, AngularForce, Opportunity) {
